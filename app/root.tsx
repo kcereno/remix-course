2c589194af7f12fb4f65d2216d2c6e8114e2677e
Karl Cereno <kagc.dev@gmail.com>
2023-07-08 13:47:03 -0400

    Finish styling

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import {
   ScrollRestoration,
 } from '@remix-run/react';
 
-import styles from '@/styles/main.css';
+import styles from '../styles/main.css';
 
 // export const links: LinksFunction = () => [
 //   ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,7 +1,15 @@
+import NewNote, { links as newNoteLinks } from 'components/NewNote/NewNote';
 import React from 'react';
 
 const NotesPage = () => {
-  return <div>notes</div>;
+  return (
+    <main>
+      <NewNote />
+      <h1>test</h1>
+    </main>
+  );
 };
 
 export default NotesPage;
+
+export const links = () => [...newNoteLinks()];
